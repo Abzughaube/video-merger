fb7c966a899dc3960b5fee8e2c267d4855840aa2
Abzughaube <53961946+Abzughaube@users.noreply.github.com>
2023-03-05 15:56:32 +0100

    fixed slider control for media player

--- VideoMerger/MediaplayerControl.xaml.cs
+++ VideoMerger/MediaplayerControl.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Xml.Linq;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace VideoMerger
 {
@@ -12,14 +18,61 @@ namespace VideoMerger
     /// </summary>
     public partial class MediaplayerControl : UserControl
     {
+        CompositeDisposable DisposableSubscriptions = new CompositeDisposable();
+
         public MediaplayerControl()
         {
             InitializeComponent();
+            _timelineSliderObservable = Observable
+                .FromEventPattern<RoutedPropertyChangedEventHandler<double>, RoutedPropertyChangedEventArgs<double>>(
+                    h => timelineSlider.ValueChanged += h,
+                    h => timelineSlider.ValueChanged -= h);
+            DisposableSubscriptions.Add(_timelineSliderObservable
+                .Where(predicate => IsSliderMovedByUser)
+                .Buffer(TimeSpan.FromMilliseconds(500)).Subscribe(onNext));
+
+            timerVideoTime = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(250)
+            };
+            timerVideoTime.Tick += new EventHandler(timer_Tick);
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            timelineSlider.Value = mediaPlayer.Position.TotalMilliseconds;
+        }
+
+        private void onNext(IList<EventPattern<RoutedPropertyChangedEventArgs<double>>> values)
+        {
+            if (!values.Any())
+            {
+                return;
+            }
+
+            var newValue = values.Last().EventArgs.NewValue;
+
+            this.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                try
+                {
+                    mediaPlayer.Position = TimeSpan.FromMilliseconds(newValue);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }));
+
         }
 
         public static readonly DependencyProperty FilePathProperty =
              DependencyProperty.Register("FilePath", typeof(string), typeof(MediaplayerControl), new
                 PropertyMetadata("", new PropertyChangedCallback(OnFilePathChanged)));
+
+        private readonly IObservable<EventPattern<RoutedPropertyChangedEventArgs<double>>> _timelineSliderObservable;
+        private DispatcherTimer timerVideoTime;
 
         public string FilePath
         {
@@ -38,36 +91,69 @@ namespace VideoMerger
         {
             mediaPlayer.Stop();
             mediaPlayer.Source = new Uri(e.NewValue as string);
-            mediaPlayer.MediaOpened += MediaPlayer_MediaOpened;
 
-            Binding binding = new Binding("Position"); // CH: does not work because Position does not change while the video is runnig
-            binding.Source = mediaPlayer;               // CH: the other way around it's also not working because there are triggered to many events to be processed per sec
-            binding.Mode = BindingMode.TwoWay;      // CH: last problem: the slider does not move the value to the position the user moved to cursor to but the slider adds 1/removes 1 depending if the cursor is after or before the slider handle
-            binding.Converter = new MillisecondsToMediaElementPositionConverter();
-            BindingOperations.ClearBinding(timelineSlider, Slider.ValueProperty);
+            //Binding binding = new Binding("Position"); // CH: does not work because Position does not change while the video is runnig
+            //binding.Source = mediaPlayer;               // CH: the other way around it's also not working because there are triggered to many events to be processed per sec
+            //binding.Mode = BindingMode.TwoWay;      // CH: last problem: the slider does not move the value to the position the user moved to cursor to but the slider adds 1/removes 1 depending if the cursor is after or before the slider handle
+            //binding.Converter = new MillisecondsToMediaElementPositionConverter();
+            //BindingOperations.ClearBinding(timelineSlider, Slider.ValueProperty);
 
-            timelineSlider.SetBinding(Slider.ValueProperty, binding);
+            //timelineSlider.SetBinding(Slider.ValueProperty, binding);
         }
 
-        private void MediaPlayer_MediaOpened(object sender, RoutedEventArgs e)
-        {
-            timelineSlider.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
-            mediaPlayer.MediaOpened -= MediaPlayer_MediaOpened;
-        }
-
+        public bool DidUserPlayVideo { get; private set; }
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
             mediaPlayer.Play();
+            timerVideoTime.Start();
+            DidUserPlayVideo = true;
         }
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
         {
             mediaPlayer.Pause();
+            timerVideoTime.Stop();
+            DidUserPlayVideo = false;
         }
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
             mediaPlayer.Stop();
+            timerVideoTime.Stop();
+            timelineSlider.Value = 0;
+            DidUserPlayVideo = false;
+        }
+
+        private void MediaPlayer_OnMediaOpened(object sender, RoutedEventArgs e)
+        {
+            timelineSlider.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
+
+            timerVideoTime.Start();
+        }
+
+        private void MediaPlayer_OnMediaEnded(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+            timerVideoTime.Stop();
+            timelineSlider.Value = 0;
+            DidUserPlayVideo = false;
+        }
+
+        public bool IsSliderMovedByUser { get; private set; }
+
+        private void TimelineSlider_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.mediaPlayer.Pause();
+            IsSliderMovedByUser = true;
+        }
+
+        private void TimelineSlider_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (DidUserPlayVideo)
+            {
+                this.mediaPlayer.Play();
+            }
+            IsSliderMovedByUser = false;
         }
     }
 
